2b2884f1b9666280be0d9301164050bc789fb532
Krut Roman <test@yopmail.com>
2017-12-01 16:05:47 +0200

    Trial Hello world module

---
+++ app/code/local/Tsg/Trial/controllers/IndexController.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Krut Roman
+ * Date: 12/1/2017
+ * Time: 3:50 PM
+ */
+
+class Tsg_Trial_IndexController
+    extends Mage_Core_Controller_Front_Action
+{
+    public function indexAction()
+    {
+        echo "Hello Magento";
+    }
+}
