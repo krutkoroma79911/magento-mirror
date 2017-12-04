9efcd2b447495dca9343092b67b9386c9f9860a1
Krut Roman <test@yopmail.com>
2017-12-04 13:39:40 +0200

    Trial Hello world module

--- app/code/local/Tsg/Trial/Block/Advertising.php
+++ app/code/local/Tsg/Trial/Block/Advertising.php
@@ -7,10 +7,33 @@
  */
 
 class Tsg_Trial_Block_Advertising
-    extends Mage_Core_Block_Template
+    extends Mage_Core_Block_Template //Mage_Core_Block_Abstract
 {
+    /**
+     * @var Mage_Catalog_Model_Product|null
+     */
+    private $product;
+
+    /**
+     * Returns current product
+     *
+     * @return Mage_Catalog_Model_Product|mixed|null
+     */
+    public function getProduct()
+    {
+        if (null === $this->product)
+        {
+            $this->product = Mage::registry('current_product');
+        }
+
+        return $this->product;
+    }
+
+    /**
+     * @return string
+     */
     public function getAdvertising()
     {
-        return "Здесь могла бы быть ваша реклама";
+       return "Здесь могла бы быть ваша реклама";
     }
 }
